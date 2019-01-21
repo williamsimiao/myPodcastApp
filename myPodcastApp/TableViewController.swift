910909895f4426e21051d8427a45eadf2f170b2c
William <williamsimiao@gmail.com>
2019-01-21 17:29:14 -0200

    removendo o VC original

--- myPodcastApp/TableViewController.swift
+++ myPodcastApp/TableViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TableViewController: UITableViewController {
 
+    var player:AVPlayer?
+    var playerItem:AVPlayerItem?
+    let valor = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,7 +36,42 @@ class TableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let murl = URL(string: "https://s3.amazonaws.com/kargopolov/kukushka.mp3")
+        player_setup(url: murl!)
+        player?.play()
+    }
+    
+    func player_setup(url:URL) {
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+        
+        let playerLayer=AVPlayerLayer(player: player!)
+        playerLayer.frame=CGRect(x:0, y:0, width:10, height:50)
+        self.view.layer.addSublayer(playerLayer)
+    }
+    
+    @IBAction func back_action(_ sender: Any) {
+        let currentTime = player?.currentItem?.currentTime()
+        let jump = CMTimeMakeWithSeconds(CMTimeGetSeconds(currentTime!) - Double(valor), preferredTimescale: currentTime!.timescale)
+        player?.seek(to: jump)
+    }
+    
+    @IBAction func play_action(_ sender: Any) {
+        if player?.rate == 0 {
+            player?.play()
+        } else {
+            player?.pause()
+        }
+    }
+    
+    @IBAction func foward_action(_ sender: Any) {
+        let currentTime = player?.currentItem?.currentTime()
+        let jump = CMTimeMakeWithSeconds(CMTimeGetSeconds(currentTime!) + Double(valor), preferredTimescale: currentTime!.timescale)
+        player?.seek(to: jump)
+    }
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
---
+++ myPodcastApp/comunication.swift
@@ -0,0 +1,12 @@
+//
+//  comunication.swift
+//  myPodcastApp
+//
+//  Created by William on 21/01/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
