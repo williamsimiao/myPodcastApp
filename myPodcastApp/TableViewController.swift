74bf01d08f39a63615c0494f5e3907be592e1d80
William <williamsimiao@gmail.com>
2019-01-22 11:05:20 -0200

    colocando o titulo do podcast

--- myPodcastApp/TableViewController.swift
+++ myPodcastApp/TableViewController.swift
@@ -17,7 +17,8 @@ class TableViewController: UITableViewController {
     var player:AVPlayer?
     var playerItem:AVPlayerItem?
     let valor = 5
-    var arrRes = [[String:AnyObject]]()
+    var arrEpisodes = [[String:AnyObject]]()
+    var dictShow = [String:AnyObject]()
     @IBOutlet var tblEpisodes: UITableView!
     
     override func viewDidLoad() {
@@ -29,15 +30,28 @@ class TableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
-        Alamofire.request("https://api.spreaker.com/v2/").responseJSON { (responseData) -> Void in
+        //Episode data
+        Alamofire.request("https://api.spreaker.com/v2/shows/2885428/episodes").responseJSON { (responseData) -> Void in
             if((responseData.result.value) != nil) {
                 let swiftyJsonVar = JSON(responseData.result.value!)
                 
-                if let resData = swiftyJsonVar["contacts"].arrayObject {
-                    self.arrRes = resData as! [[String:AnyObject]]
+                if let episodesData = swiftyJsonVar["response"]["items"].arrayObject {
+                    self.arrEpisodes = episodesData as! [[String:AnyObject]]
+                    print(self.arrEpisodes)
                 }
-                if self.arrRes.count > 0 {
+                if self.arrEpisodes.count > 0 {
                     self.tblEpisodes.reloadData()
+                }
+            }
+        }
+        
+        //Show data
+        Alamofire.request("https://api.spreaker.com/v2/shows/2885428").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                if let showData = swiftyJsonVar["response"]["show"].dictionaryObject {
+                    self.dictShow = showData as [String:AnyObject]
+                    self.navigationItem.title = self.dictShow["title"] as? String
                 }
             }
         }
@@ -94,13 +108,10 @@ class TableViewController: UITableViewController {
         let jump = CMTimeMakeWithSeconds(CMTimeGetSeconds(currentTime!) + Double(valor), preferredTimescale: currentTime!.timescale)
         player?.seek(to: jump)
     }
-    
-//    https://www.spreaker.com/show/2885428/episodes/feed
-//    https://api.spreaker.com/v2/shows/2885428/episodes
-    
+        
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        var dict = arrRes[indexPath.row]
+        var dict = arrEpisodes[indexPath.row]
         cell.textLabel?.text = dict["title"] as? String
         cell.detailTextLabel?.text = dict["published_at"] as? String
         return cell
