351a759cebd003c841ad494d1069a5206292fd2d
William <williamsimiao@gmail.com>
2019-03-14 16:09:43 -0300

    better player layout

--- myPodcastApp/ViewControllerFromTabBar/custom with xib/resumoCollectionViewCell.swift
+++ myPodcastApp/ViewControllerFromTabBar/custom with xib/resumoCollectionViewCell.swift
@@ -15,7 +15,10 @@ class resumoCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
 //        coverImg.image = UIImage(named: "cover_placeholder")!
+        titleLabel.isHidden = true
         coverImg.layer.cornerRadius = 10
+        coverImg.layer.borderWidth = 1
+        coverImg.layer.borderColor = UIColor.white.cgColor
         coverImg.clipsToBounds = true
     }
 }
